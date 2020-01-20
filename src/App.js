39c02c2b7f811ab13bf92d23dd3a471caeba00e7
Sihyun Kim <legitcode267@gmail.com>
2020-01-19 20:35:46 -0700

    Add calender to router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Route, Redirect } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Project from './components/Project';
+import Calender from './components/Calender';
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
       <Route path="/home" exact component={Home} />
       <Route path="/about" exact component={About} />
       <Route path="/project" exact component={Project} />
-      <Route path="/calender" exact component={About} />
+      <Route path="/calender" exact component={Calender} />
     </HashRouter>
   )
 }
