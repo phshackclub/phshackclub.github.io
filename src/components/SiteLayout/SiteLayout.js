8fecb1fa38f3d3276f33cb5a4a892073c35a3a6c
Sihyun Kim <legitcode267@gmail.com>
2020-01-19 20:26:36 -0700

    Fix class component

--- src/components/SiteLayout/SiteLayout.js
+++ src/components/SiteLayout/SiteLayout.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link, useLocation } from "react-router-dom";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import { Link } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 
@@ -7,52 +8,63 @@ const { Header, Content, Footer, Sider } = Layout;
 
 const year = new Date().getFullYear();
 
-const SiteLayout = ({ children }) => {
+class SiteLayout extends Component {
+  state = {
+    collapsed: false,
+  };
 
-  const location = useLocation();
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
 
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" selectedKeys={location.pathname}>
-          <Menu.Item key="/home">
-            <Link to="/home">
-              <Icon type="home" />
-              <span>Home</span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/about">
-            <Link to="/about">
-              <Icon type="file" />
-              <span>About</span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="desktop" />
-            <span>Projects</span>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Icon type="calendar" />
-            <span>Calender</span>
-          </Menu.Item>
+  render() {
+
+    const { match, location, history } = this.props;
+
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" selectedKeys={location.pathname} mode="inline">
+            <Menu.Item key="/home">
+              <Link to="/home">
+                <Icon type="home" />
+                <span>Home</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="/about">
+              <Link to="/about">
+                <Icon type="file" />
+                <span>About</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="desktop" />
+              <span>Projects</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="calendar" />
+              <span>Calender</span>
+            </Menu.Item>
           </Menu>
         </Sider>
         <Layout>
-        <Header style={{ background: '#fff', padding: 0 }} />
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-            {children}
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>PHS Hack Club ©{year}</Footer>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              {this.props.children}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>PHS Hack Club ©{year}</Footer>
+        </Layout>
       </Layout>
-    </Layout>
-  )
+    );
+  }
 }
 
-export default SiteLayout;
+export default withRouter(SiteLayout);
