ecd5809a7c53e4aabae5e35c1802d5fb9c2a89b8
Sihyun Kim <legitcode267@gmail.com>
2020-01-20 15:37:37 -0700

    Update Project.js

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import SiteLayout from '../SiteLayout';
+import { Card } from 'antd';
 
 const Project = () => {
+    
+    const { Meta } = Card;
+    
     return (
         <SiteLayout>
-            Project
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://via.placeholder.com/150" />}
+              >
+                <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
+            </Card>
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://via.placeholder.com/150" />}
+              >
+                <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
+            </Card>
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://via.placeholder.com/150" />}
+              >
+                <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
+            </Card>
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://via.placeholder.com/150" />}
+              >
+                <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
+            </Card>
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://via.placeholder.com/150" />}
+              >
+                <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
+            </Card>
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://via.placeholder.com/150" />}
+              >
+                <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
+            </Card>
         </SiteLayout>
     )
 }
