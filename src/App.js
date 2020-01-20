c803d23a95ae6921d5f3ab7ee3e388732038ae79
Sihyun Kim <legitcode267@gmail.com>
2020-01-20 16:32:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Project from './components/Project';
 import Calendar from './components/Calendar';
+import ErrorPage from './components/ErrorPage';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Route path="/about" exact component={About} />
       <Route path="/project" exact component={Project} />
       <Route path="/calendar" exact component={Calendar} />
+      <Route path="/404" exact component={ErrorPage} />
     </Router>
   )
 }
