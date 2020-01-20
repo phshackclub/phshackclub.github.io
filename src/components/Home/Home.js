6fd3687d78e1fe07adfbda19ab6402044e75ce5c
Sihyun Kim <legitcode267@gmail.com>
2020-01-20 15:27:55 -0700

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import SiteLayout from '../SiteLayout';
+import { Carousel } from 'antd';
+import './Home.css';
 
 const Home = () => {
     return (
         <SiteLayout>
-            Home
+            <Carousel autoplay>
+                <div>
+                  <h3>1</h3>
+                </div>
+                <div>
+                  <h3>2</h3>
+                </div>
+                <div>
+                  <h3>3</h3>
+                </div>
+                <div>
+                  <h3>4</h3>
+                </div>
+            </Carousel>
         </SiteLayout>
     )
 }
