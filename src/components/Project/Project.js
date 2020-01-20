e4ff22f99c122dd5ef70572346e505d962c3a2ce
Sihyun Kim <legitcode267@gmail.com>
2020-01-19 20:29:00 -0700

    Add project

---
+++ src/components/Project/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SiteLayout from '../SiteLayout';
+
+const Project = () => {
+    return (
+        <SiteLayout>
+            About
+        </SiteLayout>
+    )
+}
+
+export default Project;
