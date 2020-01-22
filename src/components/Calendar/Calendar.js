7f6003e9565fa9216dbf69de8d15627329d5e5b8
Sihyun Kim <legitcode267@gmail.com>
2020-01-21 21:27:52 -0700

    Update Calendar.js

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -6,8 +6,8 @@ const Calendar = () => {
     return (
         <SiteLayout>
             <h1>Calendar</h1>
-            <div>
-                <Iframe url="https://calendar.google.com/calendar/embed?src=psdschools.org_2b1g6ji9tv9hes33vq363gcaac%40group.calendar.google.com&ctz=America%2FDenver" styles={{ border: 0 }} width="800" height="600" frameborder="0" scrolling="no" />
+            <div style={{ margin: '16px 0' }}>
+                <Iframe url="https://calendar.google.com/calendar/embed?src=psdschools.org_2b1g6ji9tv9hes33vq363gcaac%40group.calendar.google.com&ctz=America%2FDenver" styles={{ border: 0 }} />
             </div>
         </SiteLayout>
     )
