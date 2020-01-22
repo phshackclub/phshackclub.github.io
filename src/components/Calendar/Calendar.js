99fe9a44785245df40596ee9749d5c127d30a52d
Sihyun Kim <legitcode267@gmail.com>
2020-01-21 21:20:09 -0700

    Update Calendar.js

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Iframe from 'react-iframe';
 import SiteLayout from '../SiteLayout';
 
 const Calendar = () => {
@@ -6,7 +7,7 @@ const Calendar = () => {
         <SiteLayout>
             <h1>Calendar</h1>
             <div>
-                <iframe src="https://calendar.google.com/calendar/embed?src=psdschools.org_2b1g6ji9tv9hes33vq363gcaac%40group.calendar.google.com&ctz=America%2FDenver" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                <Iframe url="https://calendar.google.com/calendar/embed?src=psdschools.org_2b1g6ji9tv9hes33vq363gcaac%40group.calendar.google.com&ctz=America%2FDenver" styles={{ border: 0 }} width="800" height="600" frameborder="0" scrolling="no" />
             </div>
         </SiteLayout>
     )
