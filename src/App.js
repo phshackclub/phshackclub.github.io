9313cf4e78c6a4a979652f124c2ca50bdbe306f8
Sihyun Kim <legitcode267@gmail.com>
2020-01-24 14:17:54 -0700

    Fix 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Project from './components/Project';
@@ -13,7 +13,7 @@ const App = () => {
       <Route path="/about" exact component={About} />
       <Route path="/project" exact component={Project} />
       <Route path="/calendar" exact component={Calendar} />
-      <Route path="/404" exact component={ErrorPage} />
+      <Route path="/error" exact component={ErrorPage} />
     </Router>
   )
 }
