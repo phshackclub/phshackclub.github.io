138e0022fe8030584c303066a24f69803040bac6
Sihyun Kim <legitcode267@gmail.com>
2020-01-24 16:55:39 -0700

    Update Project.js

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,55 +1,26 @@
 import React from 'react';
 import SiteLayout from '../SiteLayout';
-import { Card, Col, Row } from 'antd';
+import { Card } from 'antd';
 
 const Project = () => {
     
-    const { Meta } = Card;
+    const gridStyle = {
+      width: '25%',
+      textAlign: 'center',
+    };
     
     return (
         <SiteLayout>
             <h1>Project</h1>
             <br />
-            <div style={{ padding: '30px' }}>
-                <Row gutter={16}>
-                    <Col span={6}>
-                        <Card
-                            hoverable
-                            style={{ width: 240 }}
-                            cover={<img alt="example" src="https://via.placeholder.com/150" />}
-                          >
-                            <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
-                        </Card>
-                    </Col>
-                    <Col span={7}>
-                        <Card
-                            hoverable
-                            style={{ width: 240 }}
-                            cover={<img alt="example" src="https://via.placeholder.com/150" />}
-                          >
-                            <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
-                        </Card>
-                    </Col>
-                    <Col span={7}>
-                        <Card
-                            hoverable
-                            style={{ width: 240 }}
-                            cover={<img alt="example" src="https://via.placeholder.com/150" />}
-                          >
-                            <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
-                        </Card>
-                    </Col>
-                    <Col span={7}>
-                        <Card
-                            hoverable
-                            style={{ width: 240 }}
-                            cover={<img alt="example" src="https://via.placeholder.com/150" />}
-                          >
-                            <Meta title="lorem ipsum" description="lorem ipsum dolor sit amet consetetur sadipscing elitr" />
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
+            <Card title="Card Title">
+                <Card.Grid style={gridStyle}>Content</Card.Grid>
+                <Card.Grid style={gridStyle}>Content</Card.Grid>
+                <Card.Grid style={gridStyle}>Content</Card.Grid>
+                <Card.Grid style={gridStyle}>Content</Card.Grid>
+                <Card.Grid style={gridStyle}>Content</Card.Grid>
+                <Card.Grid style={gridStyle}>Content</Card.Grid>
+            </Card>
         </SiteLayout>
     )
 }
