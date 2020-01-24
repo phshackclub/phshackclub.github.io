ec3a04ee9825e9babca46604b44eff839be44f58
Sihyun Kim <legitcode267@gmail.com>
2020-01-24 12:38:17 -0700

    Fix Calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -7,8 +7,8 @@ const Calendar = () => {
     return (
         <SiteLayout>
             <h1>Calendar</h1>
-            <div style="resp-container">
-                <Iframe url="https://calendar.google.com/calendar/embed?src=psdschools.org_2b1g6ji9tv9hes33vq363gcaac%40group.calendar.google.com&ctz=America%2FDenver" styles={{ border: 0 }} />
+            <div className="responsiveCal">
+                <Iframe url="https://calendar.google.com/calendar/embed?src=psdschools.org_2b1g6ji9tv9hes33vq363gcaac%40group.calendar.google.com&ctz=America%2FDenver" styles={{ borderWidth: 0 }} frameBorder={0} scrolling={false} />
             </div>
         </SiteLayout>
     )
