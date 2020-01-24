ffabbf1c6474ea3212895068b7531a6f3df93959
Sihyun Kim <legitcode267@gmail.com>
2020-01-24 15:16:27 -0700

    Update Calendar.js

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -6,7 +6,6 @@ import './Calendar.css';
 const Calendar = () => {
     return (
         <SiteLayout>
-            <h1>Calendar</h1>
             <div className="responsiveCal">
                 <Iframe url="https://calendar.google.com/calendar/embed?src=psdschools.org_2b1g6ji9tv9hes33vq363gcaac%40group.calendar.google.com&ctz=America%2FDenver" styles={{ borderWidth: 0 }} frameBorder={0} scrolling={false} />
             </div>
